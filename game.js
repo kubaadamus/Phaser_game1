bf9a04c1dbef0eca7a8ca934d0e8099bb36ae818
kubaadamus <jakubadamus1991@gmail.com>
2019-05-11 02:31:43 +0200

    Przed powrotem do tutka

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
 var game = new Phaser.Game(800, 600, Phaser.CANVAS);
-game.state.add('Main',Technotip.Main);
-game.state.add('Level_1',Technotip.Level_1);
+game.state.add('Main',GameManager.Main);
+game.state.add('Level_1',GameManager.Level_1);
 game.state.start('Main');
--- gameStateManager.js
+++ gameStateManager.js
@@ -1,10 +1,12 @@
-var Technotip = {};
+var GameManager = {};
 
-Technotip.Main = function (game) {
+
+
+GameManager.Main = function (game) {
 };
-Technotip.Level_1 = function (game) {
+GameManager.Level_1 = function (game) {
 };
-Technotip.Main.prototype = {
+GameManager.Main.prototype = {
 
     up: function () {
         console.log('button up', arguments);
@@ -60,7 +62,6 @@ Technotip.Main.prototype = {
 
     preload: function () {
 
-
         game.load.image('menu_background', 'assets/mario/images/menu_background.jpg');
         game.load.audio('menu_theme', ['assets/mario/audio/menu_theme.mp3']);
         game.load.spritesheet('button', 'assets/mario/images/default_button_sprite_sheet.png', 193, 71);
@@ -73,8 +74,7 @@ Technotip.Main.prototype = {
         background = game.add.tileSprite(0, 0, 800, 600, 'menu_background');
         // Muzyka   
         music = game.add.audio('menu_theme');
-        if(mute==0)
-        {
+        if (mute == 0) {
             music.play();
         }
         // Przyciski będą w grupie
@@ -87,7 +87,7 @@ Technotip.Main.prototype = {
         playButton = new LabelButton(this.game, 400, 100, "button", "Play", 'play', this.actionOnClick, this, 1, 0, 2);
         main_menu_button_group.add(playButton);
 
-        optionsButton = new LabelButton(this.game, 400, 200, "button", "Options", 'options', this.actionOnClick, this, 1, 0,2);
+        optionsButton = new LabelButton(this.game, 400, 200, "button", "Options", 'options', this.actionOnClick, this, 1, 0, 2);
         main_menu_button_group.add(optionsButton);
 
         howButton = new LabelButton(this.game, 400, 300, "button", "How to play", 'how', this.actionOnClick, this, 1, 0, 2);
@@ -137,7 +137,7 @@ Technotip.Main.prototype = {
 
     },
 }
-Technotip.Level_1.prototype = {
+GameManager.Level_1.prototype = {
 
     up: function () {
         console.log('button up', arguments);
@@ -160,21 +160,90 @@ Technotip.Level_1.prototype = {
     preload: function () {
 
 
-        game.load.image('menu_background', 'assets/mario/images/menu_background.jpg');
+        game.load.image('background', 'assets/mario/images/sky.jpg');
         game.load.spritesheet('button', 'assets/mario/images/default_button_sprite_sheet.png', 193, 71);
         game.load.audio('button_click', 'assets/mario/audio/button_click.mp3');
+
+        //player sprite
+        game.load.image('mario', 'assets/mario/images/mario.png');
+
+        //Elementy otoczenia
+        game.load.image('block1', 'assets/mario/images/block1.jpg');
     },
     create: function () {
         // Tło
-        background = game.add.tileSprite(0, 0, 800, 600, 'menu_background');
+        background = game.add.tileSprite(0, 0, 800, 600, 'background');
+        background.scale.setTo(2, 2);
         //HUD
         quitToMainButton = new LabelButton(this.game, 400, 100, "button", "QUIT", 'quitToMain', this.actionOnClick, this, 1, 0, 2);
-        if(mute==0)
-        {
+        if (mute == 0) {
             music.stop();
             music.play();
         }
+        //Rejestrujemy przyciski inputu
+        //  Register the keys.
+        this.leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+        this.rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+        this.spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        //Dodaj do klawiszy przechowywanie poprzedniego stanu
+        this.leftKey.previousState = false; // false = not pressed
+        //  Stop the following keys from propagating up to the browser
+        game.input.keyboard.addKeyCapture([Phaser.Keyboard.LEFT, Phaser.Keyboard.RIGHT, Phaser.Keyboard.SPACEBAR]);
+
+        //Dodajemy do gry sprajta gracza
+        this.mario = game.add.sprite(100, 96, 'mario');
+
+        //Dodajemy sprajta bloku
+        this.tilesprite = game.add.tileSprite(100, 200, 170, 170, 'block1');
+
+        //Włączamy system fizyki
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        //  Set the world (global) gravity
+        game.physics.arcade.gravity.y = 500;
+
+        //Włączanie fizyki dla obiektu
+        //game.physics.arcade.enable([this.mario, this.tilesprite]);
+
+        //Włączamy kolizję z krawędzią świata
+        this.mario.body.collideWorldBounds = true;
+        this.mario.body.bounce.y = 0.3;
+
+
+
+        //Każemy kamerze podążać za graczem
+        game.camera.follow(this.mario);
+
+
+        game.world.setBounds(0, 0, 1920, 1200);
+
     },
     update: function () {
-    }
+
+
+        //Obsługa przycisków
+        if (this.leftKey.isDown) {
+            this.mario.body.velocity.x = -150;
+        }
+        else if (this.rightKey.isDown) {
+            this.mario.body.velocity.x = 150;
+        }
+        else if (!this.leftKey.isDown && !this.rightKey.isDown) {
+            this.mario.body.velocity.x = 0;
+        }
+
+
+
+        if (this.spaceKey.isDown && this.mario.body.onFloor()) {
+            this.mario.body.velocity.y = -250;
+        } 
+
+        //Obsługa kolizji
+        //game.physics.arcade.collide(this.mario, this.tilesprite);
+
+    },
+
+
+
+
 };
